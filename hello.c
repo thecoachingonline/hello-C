d05b26c5d6140d043e2bebff55199910e320b59f
Y.Nanthachai <thecoachingonline@gmail.com>
2024-01-28 05:49:57 +0000

    Hash

--- hello.c
+++ hello.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+
+unsigned long hash(unsigned char *str) {
+    unsigned long hash = 5381;
+    int c;
+
+    while (c = *str++) {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+
+    return hash;
+}
 
 int main(void) {
-    printf("Hello, world!\n");
+    char message[] = "สวัสดี ชาวโลก!\n";
+    unsigned long hashValue = hash(message);
+
+    printf("ข้อความเข้ารหัส: %lu", hashValue);
     return 0;
 }
